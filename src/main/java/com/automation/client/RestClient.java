7c2b78209152963076ad5945558c7827149b09bb
Lalit Kumar <ste.lalitkumar@gmail.com>
2021-09-14 21:17:44 +0530

    Update GetAPITest
    Refactoring GetAPITest

--- src/main/java/com/automation/client/RestClient.java
+++ src/main/java/com/automation/client/RestClient.java
@@ -1,40 +1,23 @@
 package com.automation.client;
 
-import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 public class RestClient {
 
     //1. Get method
-    public void getResponse(String url) throws IOException {
+    public CloseableHttpResponse getResponse(String url) throws IOException {
         CloseableHttpClient httpClient =   HttpClients.createDefault(); //Creating http connection
         HttpGet httpGet = new HttpGet(url); //http get request
         CloseableHttpResponse closeableHttpResponse =  httpClient.execute(httpGet); //hit the request as similar to Postman Send button
 
-       // a. Status code
-        int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
-        System.out.println("Status Code: " +statusCode);
+        return closeableHttpResponse;
 
-       // b. JSON String
-        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
-        JSONObject responseJson = new JSONObject(responseString);
-        System.out.println("Response JSON from API" +responseJson);
 
-       // c. All headers
-        Header[] headersArray = closeableHttpResponse.getAllHeaders();
-        HashMap<String, String> allHeaders = new HashMap<String, String>();
-        for (Header header: headersArray) {
-            allHeaders.put(header.getName(), header.getValue());
-        }
-        System.out.println("Headers Array: "+allHeaders);
     }
 
 
--- src/test/java/com/automation/test/GetAPITest.java
+++ src/test/java/com/automation/test/GetAPITest.java
@@ -2,10 +2,16 @@ package com.automation.test;
 
 import com.automation.base.TestBase;
 import com.automation.client.RestClient;
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class GetAPITest extends TestBase {
     String url;
@@ -23,7 +29,25 @@ public class GetAPITest extends TestBase {
     @Test
     public void getTest() throws IOException {
         RestClient restClient = new RestClient();
-        restClient.getResponse(url);
+        CloseableHttpResponse closeableHttpResponse = restClient.getResponse(url);
+
+        // a. Status code
+        int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+        System.out.println("Status Code: " +statusCode);
+        Assert.assertEquals(statusCode, 200, "Status code isn't 200");
+
+        // b. JSON String
+        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+        JSONObject responseJson = new JSONObject(responseString);
+        System.out.println("Response JSON from API" +responseJson);
+
+        // c. All headers
+        Header[] headersArray = closeableHttpResponse.getAllHeaders();
+        HashMap<String, String> allHeaders = new HashMap<String, String>();
+        for (Header header: headersArray) {
+            allHeaders.put(header.getName(), header.getValue());
+        }
+        System.out.println("Headers Array: "+allHeaders);
     }
 
 
